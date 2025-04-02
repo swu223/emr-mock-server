c1c6e549acb9fb4ef8817ab41551d41472b068b0
swu223 <sarahwu223@gmail.com>
2025-04-02 16:21:06 -0400

    added code for connection from node post-gres tutorial

--- db.mjs
+++ db.mjs
@@ -1,1 +1,29 @@
-//connect to the database
+// connect to the database
+// copied from https://node-postgres.com/features/connecting
+
+import pg from 'pg'
+const { Pool, Client } = pg
+
+const pool = new Pool({
+  user: 'dbuser',
+  password: 'secretpassword',
+  host: 'database.server.com',
+  port: 3211,
+  database: 'mydb',
+})
+ 
+console.log(await pool.query('SELECT NOW()'))
+ 
+const client = new Client({
+  user: 'dbuser',
+  password: 'secretpassword',
+  host: 'database.server.com',
+  port: 3211,
+  database: 'mydb',
+})
+ 
+await client.connect()
+ 
+console.log(await client.query('SELECT NOW()'))
+ 
+await client.end()
