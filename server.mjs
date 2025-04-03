1f695b1c59f348eeb15f2e40d84d469f6d71b08f
swu223 <sarahwu223@gmail.com>
2025-04-03 10:11:50 -0400

    spacing

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import pg from 'pg'
 
 const app = express();
 const PORT = 3000;
@@ -7,8 +8,32 @@ app.get('/', (req,res) => {
   res.send('Hello server folks')
 })
 
-
-
 app.listen(PORT, ()=>{
   console.log(`emr server running on PORT:${PORT}`)
 })
+
+const { Pool, Client } = pg
+
+const pool = new Pool({
+  user: 'dbuser',
+  password: 'secretpassword',
+  host: 'database.server.com',
+  port: 3211,
+  database: 'mydb',
+})
+ 
+console.log(await pool.query('SELECT NOW()'))
+ 
+const client = new Client({
+  user: 'dbuser',
+  password: 'secretpassword',
+  host: 'database.server.com',
+  port: 3211,
+  database: 'mydb',
+})
+ 
+await client.connect()
+ 
+console.log(await client.query('SELECT NOW()'))
+ 
+await client.end()
